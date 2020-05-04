e01ed43e5937ab7a2cc473c822f50e4bc5433c71
centripio <andresdigital3000@gmail.com>
2020-05-04 13:58:15 -0500

    Creando dominio

---
+++ src/main/java/com/soccer/Game.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soccer;
+
+/**
+ *
+ * @author andresbedoya
+ */
+public class Game {
+    Team local;
+    Team visitor;
+    Point[] point;
+    
+}
